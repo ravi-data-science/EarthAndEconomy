194d21121d558864bce97dc239a32fb2ec31f14a
Ravi Kiran Krovvidi <ravi.data.science@gmail.com>
2024-12-05 22:27:55 -0800

    Update reverse_list.py

--- reverse_list.py
+++ reverse_list.py
@@ -1,5 +1,10 @@
-def reverseList(self, head: ListNode) -> ListNode:
-        prev, curr = None, head
+class LinkedList()
+  def __init(self, data):
+          self.data = data
+          self.next = None
+        
+  def reverseList(self):
+        prev, curr = None, self.head
 
         while curr:
             temp = curr.next
@@ -9,5 +14,11 @@ def reverseList(self, head: ListNode) -> ListNode:
         return prev
 
 if __name__ == '__main__':
-  lst = [1,2,3,4]
-  print(reverseList(lst))
+  # lst = [1,2,3,4]
+  ll = LinkedList()   
+  ll.append(1)
+  ll.append(2)
+        
+  print(ll)
+  ll.reverseList()
+  print(ll)      
